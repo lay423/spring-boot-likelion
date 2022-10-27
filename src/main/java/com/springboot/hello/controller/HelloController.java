9d2ef78c467967392b69229eb2107472cdb97099
lay423 <junpang486@gmail.com>
2022-10-27 13:19:16 +0900

    Request3

--- src/main/java/com/springboot/hello/controller/HelloController.java
+++ src/main/java/com/springboot/hello/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.springboot.hello.controller;
 
+import com.springboot.hello.domain.dto.MemberDto;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -9,40 +10,45 @@ import java.util.Map;
 public class HelloController {
 
     @RequestMapping(name = "/hello", method = RequestMethod.GET)
-    public String hello(){
+    public String hello() {
         return "HelloWorld";
     }
 
     @GetMapping("/name")
-    public String getName(){
+    public String getName() {
         return "Junha";
     }
 
     @GetMapping("/variable1/{variable}")
-    public String getVariable1(@PathVariable String variable){
+    public String getVariable1(@PathVariable String variable) {
         return variable;
     }
 
     @GetMapping("/variable2/{variable}")
-    public String getVariable2(@PathVariable String variable){
+    public String getVariable2(@PathVariable String variable) {
         return variable;
     }
 
     @GetMapping("/request1")
     public String getVariable3(@RequestParam String name,
                                @RequestParam String email,
-                               @RequestParam String organization){
+                               @RequestParam String organization) {
         return String.format("%s %s %s", name, email, organization);
     }
 
     @GetMapping("/request2")
     public String getVariable4(@RequestParam
-                               Map<String, String> param){param.entrySet().forEach(
-            (map)->{
-                System.out.printf("key:%s value:%s\n", map.getKey(), map.getValue());
-            }
-    );
+                               Map<String, String> param) {
+        param.entrySet().forEach(
+                (map) -> {
+                    System.out.printf("key:%s value:%s\n", map.getKey(), map.getValue());
+                }
+        );
         return "호출완료";
     }
 
+    @GetMapping("/request3")
+    public String getRequestParam(MemberDto memberDto) {
+        return memberDto.toString();
+    }
 }
