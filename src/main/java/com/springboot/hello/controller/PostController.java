1154bdcc4029edbc10b46aeab8775062246d37ae
lay423 <junpang486@gmail.com>
2022-10-27 14:15:25 +0900

    Post RequestBody Dto

--- src/main/java/com/springboot/hello/controller/PostController.java
+++ src/main/java/com/springboot/hello/controller/PostController.java
@@ -1,8 +1,9 @@
 package com.springboot.hello.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.springboot.hello.domain.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/post-api")
@@ -11,5 +12,18 @@ public class PostController {
     @RequestMapping(value="/domain", method= RequestMethod.POST)
     public String postExample() {
         return "Hello Post API";
+    }
+
+    @PostMapping("/member")
+    public String postMember(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder();
+
+        postData.entrySet().forEach(map->sb.append(map.getKey()+":"+map.getValue()+"\n"));
+        return sb.toString();
+    }
+
+    @PostMapping("/member2")
+    public String postMember(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
     }
 }
