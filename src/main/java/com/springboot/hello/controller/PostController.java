a8fab0db23c19f2b0736ca6d368a026f3f79dfc5
lay423 <junpang486@gmail.com>
2022-10-27 14:35:46 +0900

    Post RequestBody Dto

--- src/main/java/com/springboot/hello/controller/PostController.java
+++ src/main/java/com/springboot/hello/controller/PostController.java
@@ -1,6 +1,8 @@
 package com.springboot.hello.controller;
 
 import com.springboot.hello.domain.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -25,5 +27,12 @@ public class PostController {
     @PostMapping("/member2")
     public String postMember(@RequestBody MemberDto memberDto) {
         return memberDto.toString();
+    }
+
+    @PostMapping("/member3")
+    public ResponseEntity<MemberDto> putMember(@RequestBody MemberDto memberDto) {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(memberDto);
     }
 }
