04b60e0453c4dafccb45c31804f9f3b4ef371495
lay423 <junpang486@gmail.com>
2022-10-28 13:44:00 +0900

    jdbc template add(), deleteAll() 추가

---
+++ src/main/java/com/springboot/hello/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.springboot.hello.controller;
+
+import com.springboot.hello.dao.UserDao;
+import com.springboot.hello.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+    private final UserDao userDao;
+
+    public UserController(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @GetMapping("/user")
+    public User addAndGet() throws SQLException {
+        userDao.add(new User("1", "Kyeongrok", "1234"));
+        return userDao.findById("1");
+    }
+
+    @DeleteMapping("/user")
+    public ResponseEntity<Integer> deleteAll() {
+        return ResponseEntity
+                .ok()
+                .body(userDao.deleteAll());
+    }
+}
